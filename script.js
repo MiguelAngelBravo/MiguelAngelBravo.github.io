8a447523905cf97ee3d331ba1b5d4d2a6cf58f67
MiguelAngelBravo <miguel.bravo@cetys.edu.mx>
2018-02-09 18:26:19 +0100

    add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+  var x = "Hello World";
+  var y = "Hallo Welt";
+  
+  function appearWorter() {
+    document.getElementById("demo").innerHTML = x;
+  }
+    function disappearWorter() {
+    document.getElementById("demo").innerHTML = y;
+  }
